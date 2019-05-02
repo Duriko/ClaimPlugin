89325dd9eb8c15396eb2b5b6b3259ae3b2ed77d4
BuildTools <unconfigured@null.spigotmc.org>
2019-05-02 20:52:55 +0200

    Initial commit

---
+++ src/main/java/org/spigotmc/plugin.java
@@ -0,0 +1,16 @@
+package org.spigotmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

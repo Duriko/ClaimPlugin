5c68829441eca801bd26d10105b898e77a635ac2
BuildTools <unconfigured@null.spigotmc.org>
2019-09-29 09:49:56 +0200

    Added new claim functionality. New commands are /claim create <claimname>/removing/info/list

--- src/main/java/org/spigotmc/plugin.java
+++ src/main/java/org/spigotmc/plugin.java
@@ -1,5 +1,17 @@
 package org.spigotmc;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.RegionSelector;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.domains.DefaultDomain;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.*;
 import org.bukkit.*;
 import org.bukkit.ChatColor;
 import org.bukkit.boss.BarColor;
@@ -9,8 +21,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.*;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 public final class plugin extends JavaPlugin implements CommandExecutor {
@@ -18,22 +33,130 @@ public final class plugin extends JavaPlugin implements CommandExecutor {
     private JumpingListener jp;
     private static Player player;
 
+    public WorldEditPlugin getWorldedit(){
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+        if(plugin instanceof WorldEditPlugin)
+            return (WorldEditPlugin) plugin;
+        else
+            return null;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if(label.equalsIgnoreCase("spawn")){
-            if(sender instanceof Player) {
+        if (label.equalsIgnoreCase("claim")){
+            if (sender instanceof Player) {
                 player = (Player) sender;
-                player.teleport(new Location(player.getWorld(),player.getWorld().getSpawnLocation().getX(), player.getBedSpawnLocation().getY() ,player.getBedSpawnLocation().getZ()));
+                RegionContainer regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer();
+                RegionManager regionManager = regionContainer.get(BukkitAdapter.adapt(player.getWorld()));
+                RegionSelector regionSelector = getWorldedit().getSession(player).getRegionSelector(BukkitAdapter.adapt(player.getWorld()));
+                try {
+                    if(args[0]!=null){
+
+                        /**
+                         * To remove a claim you must stand in the claim.
+                         * To remove a claim use the command /claim remove <claimname>
+                         **/
+
+                        if (args[0].equalsIgnoreCase("remove"))
+                            if (args.length >= 2 && args[1] != null) {
+                                ApplicableRegionSet regionList = regionManager.getApplicableRegions(BlockVector3.at(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()));
+                                for (ProtectedRegion region : regionList) {
+                                    if (region.getId().equalsIgnoreCase("claim_" + player.getName() + "_" + args[1])) {
+                                        regionManager.removeRegion("claim_" + player.getName() + "_" + args[1]);
+                                        player.sendMessage(ChatColor.YELLOW + "Claim " + args[1] + " has been removed!");
+                                        return true;
+                                    } else {
+                                        player.sendMessage(ChatColor.YELLOW + "No claim with that name found! " + "claim_" + player.getName() + "_" + args[1]);
+                                        return false;
+                                    }
+                                }
+                                player.sendMessage(ChatColor.YELLOW+"No claim with the name: " + args[1] + " exists!");
+                                return false;
+                            } else {
+                                player.sendMessage(ChatColor.YELLOW+"You need to specify the claim name!");
+                                return false;
+                            }
+                        /**
+                         * Get a list of players claims
+                         **/
+                        if (args[0].equalsIgnoreCase("list")){
+                            List<ProtectedRegion> playersClaim = new ArrayList<>();
+                            player.sendMessage(ChatColor.YELLOW+"Your claims:");
+                            for (ProtectedRegion region : regionManager.getRegions().values()){
+                                if (region.getId().contains("claim_"+player.getName())) {
+                                    playersClaim.add(region);
+                                    player.sendMessage(ChatColor.YELLOW + region.getId());
+                                }
+                            }
+                        }
+
+                        /**
+                         * To show information about a claim, the player must stand
+                         * in the claim and use the /claim info command
+                         **/
+
+                        if (args[0].equalsIgnoreCase("info")){
+                            ApplicableRegionSet regionList = regionManager.getApplicableRegions(BlockVector3.at(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()));
+                            for (ProtectedRegion region : regionList) {
+                                if (region.getOwners().contains(player.getName())) {
+                                    player.sendMessage(ChatColor.YELLOW + "Claim information:");
+                                    player.sendMessage(ChatColor.YELLOW + "Claim id: " + region.getId());
+                                    player.sendMessage(ChatColor.YELLOW + "Claim coords: " + region.getMinimumPoint() + " - " + region.getMaximumPoint());
+                                    String tmp = "";
+                                    Map<Flag<?>, Object> map = region.getFlags();
+                                    for (Flag<?> flag : region.getFlags().keySet()) {
+                                        map.get(flag);
+                                        tmp += flag.getName() + ": " + map.get(flag) + "; ";
+                                    }
+                                    player.sendMessage(ChatColor.YELLOW + "Claim flags: " + tmp);
+                                    player.sendMessage(ChatColor.YELLOW + "Claim owner: " + region.getOwners().getPlayers());
+                                    player.sendMessage(ChatColor.YELLOW + "Claim members: " + region.getMembers().getPlayers());
+                                    return true;
+                                }
+                            }
+                        }
+
+                        /**
+                         * To create a claim, the player must first create a selection
+                         * A selection must be made using the Worldedit wand.
+                         * When a selection has been made, use the command /claim create <claimname>
+                         * to create a new claim with the name claim_<playerName>_claimname
+                         **/
+                        if (args[0].equalsIgnoreCase("create")){
+                            try {
+                                BlockVector3 p1 = regionSelector.getRegion().getMinimumPoint();
+                                BlockVector3 p2 = regionSelector.getRegion().getMaximumPoint();
+                                for(ProtectedRegion region : regionManager.getRegions().values()){
+                                    if(region.contains(p1) || region.contains(p2)){
+                                        player.sendMessage(ChatColor.YELLOW+"Claim overlaps with another claim! (" + region.getId()+")");
+                                        return false;
+                                    }
+                                }
+                                if(regionManager.getRegion("claim_"+player.getName()+"_"+args[0]) == (null)){
+                                    regionManager.getRegion("claim_"+player.getName()+"_"+args[0]);
+                                    ProtectedRegion region = new ProtectedCuboidRegion("claim_"+player.getName()+"_"+args[0],
+                                            BlockVector3.at(p1.getBlockX(), 0, p1.getBlockZ()), BlockVector3.at(p2.getBlockX(), 255, p2.getBlockZ()));
+                                    regionManager.addRegion(region);
+                                    region.setFlag(Flags.PVP, StateFlag.State.DENY);
+                                    region.setFlag(Flags.BUILD, StateFlag.State.DENY);
+                                    DefaultDomain owner = region.getOwners();
+                                    owner.addPlayer(player.getName());
+                                    region.setOwners(owner);
+                                    player.sendMessage("Claim " + "claim_"+player.getName()+"_"+args[0] + " created!");
+                                } else
+                                    player.sendMessage("Claim with that name already exist");
+                            } catch (NullPointerException ne) { }
+                            catch (Exception ex){ }
+                        }
+                    }
+
+                }catch (Exception e){ }
             }
         }
 
-        if(label.equalsIgnoreCase("test")){
-            if(sender instanceof Player){
-                player = (Player) sender;
-                EntityType entity = EntityType.PIG;
-                player.getWorld().spawnEntity(player.getEyeLocation(), entity);
-            }
+        if(label.equalsIgnoreCase("spawn")){
+            player.teleport(new Location(player.getWorld(),player.getWorld().getSpawnLocation().getX(), player.getBedSpawnLocation().getY() ,player.getBedSpawnLocation().getZ()));
         }
 
         if (label.equalsIgnoreCase("day")) {
@@ -43,38 +166,8 @@ public final class plugin extends JavaPlugin implements CommandExecutor {
                 player.getWorld().setWeatherDuration(0);
             }
         }
-        if(label.equalsIgnoreCase("xp")) {
-            if (sender instanceof Player) {
-                player = (Player) sender;
-                player.giveExpLevels(500);
-                player.sendMessage("Test");
-            }
-        }
-        if (label.equalsIgnoreCase("bmessage")) {
-            if (args.length < 2) {
-                sender.sendMessage(ChatColor.RED + "Please specify a name and message!");
-            } else {
-                Player target = Bukkit.getPlayer(args[0]);
 
-                if(target == null) {
-                    sender.sendMessage(ChatColor.RED+args[0]+" is not online!");
-                }
-                StringBuilder x = new StringBuilder();
 
-                for (int i=1; i<args.length; i++)
-                    x.append(args[i]);
-
-                String message = x.toString().trim();
-
-                BossBar bar = Bukkit.createBossBar(ChatColor.translateAlternateColorCodes('&', message), BarColor.YELLOW, BarStyle.SOLID);
-
-                bar.addPlayer(target);
-
-                Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
-                    bar.removePlayer(target);
-                }, 200);
-            }
-        }
         return true;
     }
 
